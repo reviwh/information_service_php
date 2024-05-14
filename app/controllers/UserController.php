f0a7d9472c7f8d6e17d214a3d80961e77f79e1bf
reviwh <reviwh2@gmail.com>
2024-05-14 21:14:19 +0700

    feature: register user.

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -6,7 +6,7 @@ class UserController extends Controller
 {
     public function index($id)
     {
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             if (isset($_POST['token'])) {
                 $data = $this->repository('UserRepository')->getUser($id, $_POST['token']);
                 http_response_code($data["code"]);
@@ -34,7 +34,42 @@ class UserController extends Controller
 
     public function register()
     {
-        // TODO: Implement register() method.
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            if (
+                isset($_POST['name']) &&
+                isset($_POST['email']) &&
+                isset($_POST['no_telp']) &&
+                isset($_POST['password']) &&
+                isset($_POST['address']) &&
+                isset($_POST['role'])
+            ) {
+                if (isset($_FILES['id_card'])) {
+                    $id_card = $_FILES['id_card'];
+                    if ($id_card['type'] === 'application/pdf') {
+                        $data = $this->repository('UserRepository')->create($_POST, $id_card);
+                        http_response_code($data["code"]);
+                        $response = new Response($data['message']);
+                        echo $response->send();
+                    } else {
+                        http_response_code(400);
+                        $response = new Response("Accepted id card file is only pdf.");
+                        echo $response->send();
+                    }
+                } else {
+                    http_response_code(400);
+                    $response = new Response('ID Card is required');
+                    echo $response->send();
+                }
+            } else {
+                http_response_code(400);
+                $response = new Response('Required path parameter is not complete.');
+                echo $response->send();
+            }
+        } else {
+            http_response_code(405);
+            $response = new Response('Method not allowed, please use POST');
+            echo $response->send();
+        }
     }
 
     public function update()
--- app/repositories/UserRepository.php
+++ app/repositories/UserRepository.php
@@ -32,9 +32,50 @@ final class UserRepository
     return $result;
   }
 
-  public function register($data)
+  public function create($data, $id_card)
   {
-    // TODO: Implement register() method.
+    $result = array();
+
+    if ($this->isEmailAvailable($data['email'])) {
+      $id_card_desc = $this->saveIdCard($id_card);
+
+      if (isset($id_card_desc['path'])) {
+        $password = password_hash($data['password'], PASSWORD_BCRYPT);
+        $token = md5(md5(md5($data['email']) . $data['role']) . $password);
+
+        $this->db->query(
+          "INSERT INTO {$this->table} 
+        (name, email, password, no_telp, id_card, address, role, token) 
+        VALUES (:name, :email, :password, :no_telp, :id_card, :address, :role, :token)"
+        );
+
+        $this->db->bind('name', $data['name']);
+        $this->db->bind('email', $data['email']);
+        $this->db->bind('password', $password);
+        $this->db->bind('no_telp', $data['no_telp']);
+        $this->db->bind('id_card', $id_card_desc['path']);
+        $this->db->bind('address', $data['address']);
+        $this->db->bind('role', $data['role']);
+        $this->db->bind('token', $token);
+        $this->db->execute();
+
+        if ($this->db->rowCount() > 0) {
+          $result['code'] = 200;
+          $result['message'] = "Register success.";
+        } else {
+          $result['code'] = 400;
+          $result['message'] = "Register failed.";
+        }
+      } else {
+        $result['code'] = 400;
+        $result['message'] = "Unknown path";
+      }
+    } else {
+      $result['code'] = 400;
+      $result['message'] = "Email is unavailable.";
+    }
+
+    return $result;
   }
 
   public function getUser($id, $token)
@@ -60,7 +101,6 @@ final class UserRepository
       $result['message'] = "You're unauthorize to this request.";
     }
 
-
     return $result;
   }
 
@@ -69,10 +109,18 @@ final class UserRepository
     // TODO: Implement updateUser() method.
   }
 
-  private function getPassword($email)
+  private function isEmailAvailable($email)
   {
-    $this->db->query("SELECT password FROM {$this->table} WHERE email=:email");
+    $this->db->query("SELECT email FROM {$this->table} WHERE email=:email");
     $this->db->bind('email', $email);
+    $this->db->execute();
+    return $this->db->rowCount() <= 0;
+  }
+
+  private function getPassword($id)
+  {
+    $this->db->query("SELECT password FROM {$this->table} WHERE id=:id");
+    $this->db->bind('id', $id);
     return $this->db->single();
   }
 
@@ -82,5 +130,14 @@ final class UserRepository
     $this->db->bind('token', $token);
     $this->db->execute();
     return $this->db->rowCount() > 0;
+  }
+
+  private function saveIdCard($id_card)
+  {
+    $result = array();
+    if (move_uploaded_file($id_card['tmp_name'], __DIR__ . '/../../api/storage/user/' . date('YmdHis') . $id_card['name'])) {
+      $result['path'] = '/storage/user/' . date('YmdHis') . $id_card['name'];
+    }
+    return $result;
   }
 }
